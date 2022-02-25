dd0a5f5009a336cbe8a66c4667078f5405e2f432
peytontucker <peytonmtucker@gmail.com>
2022-02-25 17:10:09 -0500

    added hashmap instantiation to constructor

--- InputHandler.java
+++ InputHandler.java
@@ -1,7 +1,14 @@
+import java.util.HashMap;
+
 public class InputHandler {
     
-    public InputHandler(Player player) {
+    private HashMap<String, Command> commands;
 
+    public InputHandler(Player player) {
+        this.commands.put("jump", new JumpCommand(player));
+        this.commands.put("run", new RunCommand(player));
+        this.commands.put("fire", new FireCommand(player));
+        this.commands.put("quit", new QuitCommand());
     }
 
     public void buttonPressed(String command) {
